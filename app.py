e7fec58b3f26f485c634cfef9d192f0079cc8c55
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-26 14:42:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ st.markdown("""
     .css-f4ro0r {
         align-items: center !important;
     }
-    .css-1b655ro {
+    .MuiButton-root {
         background-color: #002147 !important;
         color: #FAFAFA !important;
         text-transform: capitalize !important;
