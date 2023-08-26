192ec05c9793107c944e1bd58c777cf434453560
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-26 15:34:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -809,7 +809,7 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col2:    
   webrtc_streamer(key="example", video_frame_callback=callback, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-  st.write("hello")
+  st.write("hello1")
     
 
 
