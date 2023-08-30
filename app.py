5d69ad2fb1433cdaffd493494a845356a8a59f93
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-30 09:55:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -819,9 +819,10 @@ with col2:
                 if results_detection.detections:
                     for detection in results_detection.detections:
                         (x, y, w, h) = detection.location_data.relative_bounding_box
+                        cv2.rectangle(image, (x, y), (x + w, y + h), (252, 188, 36, 0), 4)
 
-                    for detection in results_detection.detections:
-                        mp_drawing.draw_detection(image=image, detection=detection, keypoint_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
+       #             for detection in results_detection.detections:
+       #                 mp_drawing.draw_detection(image=image, detection=detection, keypoint_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
         
                 if results_mesh.multi_face_landmarks:
                     for face_landmarks in results_mesh.multi_face_landmarks:
