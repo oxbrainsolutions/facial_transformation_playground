1377b2a42f64a8ac4bc735487d7c6694668f501c
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-30 11:25:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -823,13 +823,14 @@ with col2:
                    #     (x, y, w, h) = detection.location_data.relative_bounding_box
                    #     cv2.rectangle(image, (x, y), (x + w, y + h), (252, 188, 36, 0), 4)
 
-                    cv2.putText(image, "Hello", (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+                    cv2.putText(image, "Hello", (0.5 * width, 0.5 * height), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+                    cv2.rectangle(image, (0.5 * width, 0.5 * height), (0.75 * width, 0.75 * height), (252, 188, 36), 4)
                     for detection in results_detection.detections:
                         mp_drawing.draw_detection(image=image, detection=detection, keypoint_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
-                        location_data = detection.location_data
-                        if location_data.format == LocationData.RELATIVE_BOUNDING_BOX:
-                            bb = location_data.relative_bounding_box
-                            cv2.rectangle(image, (bb.xmin * width, bb.ymin * height), (bb.xmin * width + bb.width * width, bb.ymin * height + bb.height * height), (252, 188, 36), 4)
+                    #    location_data = detection.location_data
+                    #    if location_data.format == LocationData.RELATIVE_BOUNDING_BOX:
+                    #        bb = location_data.relative_bounding_box
+                    #        cv2.rectangle(image, (bb.xmin * width, bb.ymin * height), (bb.xmin * width + bb.width * width, bb.ymin * height + bb.height * height), (252, 188, 36), 4)
         
                 if results_mesh.multi_face_landmarks:
                     for face_landmarks in results_mesh.multi_face_landmarks:
