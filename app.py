6b76d90502f6f524d737eda2fc8123bdec710dc4
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-30 12:44:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -824,9 +824,9 @@ with col2:
                         location_data = detection.location_data
                         bb = location_data.relative_bounding_box
                         cv2.rectangle(image, (int(bb.xmin * width), int(bb.ymin * height)), (int(bb.xmin * width + bb.width * width), int(bb.ymin * height + bb.height * height)), (36, 188, 252), 4)
-                      #  delta_abs = min(float(bb.xmin * width * delta), float(bb.ymin * height * delta))
-                        delta_abs = 50
-                      #  cv2.rectangle(image, (int(bb.xmin * width - delta_abs), int(bb.ymin * height - delta_abs)), (int(bb.xmin * width + bb.width * width + delta_abs), int(bb.ymin * height + bb.height * height + delta_abs)) (36, 188, 252), 4)
+                        delta_abs = min(float(bb.xmin * width * delta), float(bb.ymin * height * delta))
+                      #  delta_abs = 50
+                        cv2.rectangle(image, (int(bb.xmin * width - delta_abs), int(bb.ymin * height - delta_abs)), (int(bb.xmin * width + bb.width * width + delta_abs), int(bb.ymin * height + bb.height * height + delta_abs)), (36, 188, 252), 4)
 
  
         
