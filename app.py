2c8b5d9485dd274139e03d1b45e1fbe99e0dd00a
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-31 12:03:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ import cv2
 import av
 import mediapipe as mp
 
-from Detector import FaceDetector
-from MaskGenerator import MaskGenerator
-detector = FaceDetector()
-maskGenerator = MaskGenerator()
-target_image, target_alpha = detector.load_target_img("images/putin.png")
-target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
-target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
-
-maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-
 st.set_page_config(page_title="Facial Recognition Playground", page_icon="images/oxbrain_favicon.png", layout="wide")
 
 st.elements.utils._shown_default_value_warning=True
@@ -815,14 +805,7 @@ with col2:
         mp_drawing_styles = mp.solutions.drawing_styles
         drawing_spec = mp_drawing.DrawingSpec(color=(244, 169, 3), thickness=1, circle_radius=1)
         image = frame.to_ndarray(format="bgr24")
-
-        landmarks, image_update, face_landmarks = detector.find_face_landmarks(image)
-        detector.stabilizeVideoStream(image, landmarks)
-        output = maskGenerator.applyTargetMaskToTarget(landmarks)
-        image_out = detector.drawLandmarks(image_update, face_landmarks)
-        image = np.copy(output)
-        img_actual = np.copy(image_out)
-        
+       
         with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
             with mp_face_mesh.FaceMesh(max_num_faces=5, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
                 image.flags.writeable = False
