41b9adfd79b4bcfba788200b6e3b28d6c5a1c78d
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-01 11:28:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -805,12 +805,11 @@ with col2:
   subheader_text_field2.markdown(information_media_query + information_text1, unsafe_allow_html=True)
 
 
-st.write("hello2")
 target_image, target_alpha = detector.load_target_img("images/marilyn.png")
 target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
 target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
 maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-st.image(target_image_out)
+#st.image(target_image_out)
 col1, col2, col3 = st.columns([2, 4, 2])
 with col2:
 
