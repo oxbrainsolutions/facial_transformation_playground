be0bda287c2579db6be04bca6b12449a81714e13
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-01 13:54:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -807,7 +807,7 @@ with col2:
   subheader_text_field2 = st.empty()
   subheader_text_field2.markdown(information_media_query + information_text1, unsafe_allow_html=True)
 
-col1, col2, col3, col4, col5, col6 = st.columns([2, 1, 1, 1, 1, 2])
+col1, col2, col3, col4, col5, col6 = st.columns([2, 1, 1, 1, 0.5, 2.5])
 with col2:
     st.write("")
     st.write("")
