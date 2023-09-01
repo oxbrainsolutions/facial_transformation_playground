833e61dfe871a12978793787708323e873a2c835
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-01 11:51:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -804,8 +804,8 @@ with col2:
   subheader_text_field2 = st.empty()
   subheader_text_field2.markdown(information_media_query + information_text1, unsafe_allow_html=True)
 
-st.write("terminator2")
-target_image, target_alpha = detector.load_target_img("images/terminator.png")
+st.write("jack")
+target_image, target_alpha = detector.load_target_img("images/captain_jack.png")
 target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
 target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
 maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
