81993c13bc25fc3ffb710b798c84f00e6c8397f8
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-01 11:49:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -830,6 +830,7 @@ with col2:
             detector.stabilizeVideoStream(image, landmarks)
             image_out = detector.drawLandmarks(image, face_landmarks)
             output = maskGenerator.applyTargetMask(image, landmarks)
+    #        output = maskGenerator.applyTargetMaskToTarget(landmarks)
             return av.VideoFrame.from_ndarray(output, format="bgr24")
     
     webrtc_ctx = webrtc_streamer(key="facial-recognition", mode=WebRtcMode.SENDRECV, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}, video_frame_callback=video_frame_callback, media_stream_constraints={"video": True, "audio": False}, async_processing=True,)
