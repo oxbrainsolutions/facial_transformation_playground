d5f4e5101291c94882809b5d2bc2ed415d17b815
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-02 14:08:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -828,7 +828,7 @@ with col4:
         target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-        cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
+        cv2.rectangle(target_image_out, (-5, -5), (target_image_out.shape[1] +5, target_image_out.shape[0] +5), (252, 188, 36, 0), 30)
         st.image(target_image_out, use_column_width="always")
     if st.session_state.user_face_select == "Elvis Presley":
         target_image, target_alpha = detector.load_target_img("images/elvis.png")
