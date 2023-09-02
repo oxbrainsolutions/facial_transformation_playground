a32fb68c0b5239c00a0b60dd3e58beff310a5899
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-02 14:45:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -829,31 +829,35 @@ with col4:
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)     
         cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
-        st.image(target_image_out, use_column_width="always")
+        st.image(target_image_out, use_column_width=True)
     if st.session_state.user_face_select == "Elvis Presley":
         target_image, target_alpha = detector.load_target_img("images/elvis.png")
         target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-        st.image(target_image_out, use_column_width="always")
+        cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
+        st.image(target_image_out, use_column_width=True)
     if st.session_state.user_face_select == "Joker":
         target_image, target_alpha = detector.load_target_img("images/joker.png")
         target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-        st.image(target_image_out, use_column_width="always")
+        cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
+        st.image(target_image_out, use_column_width=True)
     if st.session_state.user_face_select == "Terminator":
         target_image, target_alpha = detector.load_target_img("images/terminator.png")
         target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-        st.image(target_image_out, use_column_width="always")
+        cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
+        st.image(target_image_out, use_column_width=True)
     if st.session_state.user_face_select == "Tom Cruise":
         target_image, target_alpha = detector.load_target_img("images/tom_cruise.png")
         target_landmarks, _, target_face_landmarks= detector.find_face_landmarks(target_image)
         target_image_out = detector.drawLandmarks(target_image, target_face_landmarks)
         maskGenerator.calculateTargetInfo(target_image, target_alpha, target_landmarks)
-        st.image(target_image_out, use_column_width="always")
+        cv2.rectangle(target_image_out, (0, 0), (target_image_out.shape[1], target_image_out.shape[0]), (252, 188, 36, 0), 30)
+        st.image(target_image_out, use_column_width=True)
     
 if boundary_toggle_switch:
     st.session_state.show_boundary = True
