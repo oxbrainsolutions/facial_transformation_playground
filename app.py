51d0b6385badab5617d3c40f644ac3533fc6e559
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-02 14:34:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -834,7 +834,7 @@ with col4:
         image = Image.open("images/brad_pitt.png")
         new_image = Image.new(image.mode, size=(image.size[0], image.size[1]))
         new_image.putdata(image.getdata())
-        new_image = ImageOps.expand(new_image, border=100, fill=(255, 255, 255))
+        new_image = ImageOps.expand(new_image, border=100, fill=(34, 34, 34))
         img_array = np.array(new_image.convert("RGB"))
         cv2.rectangle(img_array, (0, 0), (img_array.shape[1], img_array.shape[0]), (252, 188, 36, 0), 30)
       
