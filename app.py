2582302895ee3c527d4d43f234095f10007e24a7
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-03 10:41:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -966,16 +966,16 @@ with col2:
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             landmarks, image, face_landmarks = detector.find_face_landmarks(image)
-            if len(landmarks) == 0:
-                continue
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            detector.stabilizeVideoStream(image, landmarks)
-            image_out = detector.drawLandmarks(image, face_landmarks)
-            output = maskGenerator.applyTargetMask(image, landmarks)
-#           output = maskGenerator.applyTargetMaskToTarget(landmarks)
-            return av.VideoFrame.from_ndarray(output, format="bgr24")
+            if len(landmarks) == 0:
+                return av.VideoFrame.from_ndarray(image, format="bgr24")
+            else:
+                detector.stabilizeVideoStream(image, landmarks)
+                image_out = detector.drawLandmarks(image, face_landmarks)
+                output = maskGenerator.applyTargetMask(image, landmarks)
+    #           output = maskGenerator.applyTargetMaskToTarget(landmarks)
+                return av.VideoFrame.from_ndarray(output, format="bgr24")
     
         webrtc_ctx = webrtc_streamer(key="facial-recognition", mode=WebRtcMode.SENDRECV, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}, video_frame_callback=video_frame_callback, media_stream_constraints={"video": True, "audio": False}, async_processing=True,)
 
